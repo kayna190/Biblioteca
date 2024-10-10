141e345a36ff1e4f38ef1663c64cf86f490fb532
Gabriellecarv <gabriellefcventura@gmail.com>
2024-10-10 10:19:15 -0300

    adicionando projeto

---
+++ Biblioteca/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+
+Console.WriteLine("\n----- Bem Vindo a Biblioteca ----");
+
+Console.WriteLine("Solicite o nome do usuario:");
+string usuario = Console.ReadLine();
+
+while (true)
+{
+Console.WriteLine("Escolha uma opção: "); 
+Console.WriteLine("Devolução");
+Console.WriteLine("Cadastrar Livro");
+string opção = Console.ReadLine();
+
+}
+
+	}
+}
